7d365213cb02343481f1832c618056924c60f90b
sp6433057-boop <sp6433057@gmail.com>
2026-01-13 12:23:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -397,7 +397,9 @@ def logout():
 
 # ================= RUN =================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
